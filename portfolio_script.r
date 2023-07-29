5f6fb3da9309102f1208e2c5c5412364d91567d7
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-07-29 16:42:16 +0200

    save output in a new env

--- portfolio_script.r
+++ portfolio_script.r
@@ -9,101 +9,12 @@ mlc <- get_balance()
 open.price <- get_historic()
 portfolio <- get_portfolio()
 
-
-# for further use, we run: mlc <- get_balance()
-get_balance <- function () {
-    
-    library('binancer')
-    library('rjson')
-    
-    env.var <- fromJSON(file = 'variables.env.json')
-    binance_credentials(env.var[[1]],env.var[[2]])    
-
-# get binance balance without zero
-    bin.balance <- as_tibble(binance_balances())
-    bin.balance <- filter(bin.balance, bin.balance$free != 0)
-    bin.balance <- select(bin.balance, -c(free, locked))
-    bin.balance <- arrange(bin.balance, asset)
-    bin.balance <- filter(bin.balance, !(asset %in% c('ETWH', 'LUNA', 'USDC', 'XRP', 'ETHW')))
-
-    safe.balance <- read_table("safe.csv", col_names = c('asset', 'total'))
-
-# get total balance
-    balance <- bind_rows(bin.balance, safe.balance) %>%
-        group_by(asset) %>%
-        summarise(total = sum(total))
-
-    mlc.asset <- as.list(balance$asset)
-# remove USDT form our assets & add USDT to each asset
-    mlc.asset <- mlc.asset[-which(mlc.asset == 'USDT')]
-    mlc.asset.usdt <- as.list(paste(mlc.asset, 'USDT', sep = ''))
-
-    mlc <- list(balance = balance, asset.usdt = mlc.asset.usdt, asset = mlc.asset)
-    return(mlc)
-}
-
-# for further use, we run: open.price <- get_historic()
-get_historic <- function() {
-
-    # mlc <- get_balance()
-    
-    #asset.price.tb <- lapply(mlc$asset.usd, function(x, y, z) binance_klines(x,
-    #interval = '8h', start_time = '2023-07-17', end_time = '2023-07-21'))
-    asset.price.tb <- lapply(mlc$asset.usd, function(x) binance_klines(x,
-             interval = '8h'))
-    # keep open value for each day 18:00
-    keep_daily_open <- function(df) {
-    df[grepl('18:00:00', df$open_time)]
-    }
-
-    asset.price.open <- lapply(asset.price.tb, keep_daily_open)
-    names(asset.price.open) <- mlc$asset
-    # final list of df with open_time, open and symbol columns
-    asset.price.open <- map(asset.price.open, ~select(.x, 1, 2, 12))
-    # change df into tibbles
-    open.price <- map(asset.price.open, as_tibble)
-    return(open.price)
-}
-
-# we must remove row with USDT
-# balance.no.usd <- filter(balance, !asset == 'USDT')
-
-# for further use, we run: portfolio <- get_portfolio()
-get_portfolio <- function() {
-
-    mlc <- get_balance()
-    open.price <- get_historic()
-    
-    # get a asset/value tibble with open price. 
-    # we first get a list of numeric value
-    open.price.list <- lapply(open.price, function(x) pull(x,open))
-    # here we keep last daily value
-    open.price.list.last <- open.price.list |> map(c(length(open.price.list[[1]]),1))
-    # transform list into tibble
-    open.price.list.last <- open.price.list.last %>% map_dfr(~ .x %>% as_tibble(), .id = 'asset')
-
-
-    # create final tibble
-    balance.final <- inner_join(mlc$balance, open.price.list.last, by = 'asset')
-    balance.final <- rename(balance.final, 'amount' = total)
-    balance.final <- mutate(balance.final, 'total' = amount * value)
-
-    ## add a weight column
-    sum.asset <- sum(balance.final %>% pull(total))
-    balance <- mutate(balance.final, weight = total / sum.asset)
-    balance <- mutate(balance, weight = round(weight * 100, digits = 2))
-    
-    # order by weight
-    balance <- arrange(balance, desc(weight))
-    # round numbers
-    balance <-  mutate(balance, amount = round(amount, 2), value = round(value, 2), total = round(total, 2))
-    return(balance)
-}
-
 # rename portfolio with today date
 today_date <- format(Sys.Date(), "%Y%m%d")
 new_name <- paste0('portfolio_', today_date)
-assign(new_name, portfolio, envir = .GlobalEnv)
+# save it in a new env
+.PortfolioEnv <- new.env()
+assign(new_name, portfolio, envir = .PortfolioEnv)
 
 # set_names(c("foo", "bar")) |> purr::map_chr(paste0, ":suffix")
 # export to pdf
