7f66cd9ed1f16dab88f79db2f5cce00fd0bc5c26
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-07-29 17:14:49 +0200

    some cosmetic changes

--- get_historic.r
+++ get_historic.r
@@ -13,14 +13,14 @@ get_historic <- function() {
   
     #asset.price.tb <- lapply(mlc$asset.usd, function(x, y, z) binance_klines(x,
     #interval = '8h', start_time = '2023-07-17', end_time = '2023-07-21'))
-    asset.price.tb <- lapply(mlc$asset.usd, function(x) binance_klines(x,
+    asset.price.list <- lapply(mlc$asset.usd, function(x) binance_klines(x,
              interval = '8h'))
     # keep open value for each day 18:00
     keep_daily_open <- function(df) {
     df[grepl('18:00:00', df$open_time)]
     }
 
-    asset.price.open <- lapply(asset.price.tb, keep_daily_open)
+    asset.price.open <- lapply(asset.price.list, keep_daily_open)
     names(asset.price.open) <- mlc$asset
     # final list of df with open_time, open and symbol columns
     asset.price.open <- map(asset.price.open, ~select(.x, 1, 2, 12))
