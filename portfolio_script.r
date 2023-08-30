78b3195bbe837fe8f9d01d02b1bf6f56a5ffcadc
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-08-30 15:55:32 +0200

    still a work in progress

--- portfolio_script.r
+++ portfolio_script.r
@@ -5,24 +5,16 @@ source('get_balance.r')
 source('get_historic.r')
 source('get_portfolio.r')
 
+# create a new env to save our data
+.PortfolioEnv <- new.env()
+
 # set the dates
-start_time  <- format(Sys.Date()-x, "%Y-%m-%d")
+start_time  <- format(Sys.Date()-2, "%Y-%m-%d")
 end_time <- format(Sys.Date()-1, "%Y-%m-%d")
 
 mlc <- get_balance()
 open.price <- get_historic()
 portfolio <- get_portfolio()
-
-# rename portfolio with yesterday date
-yesterday_date <- gsub('-', '', end_time)
-new_name <- paste0('portfolio_', yesterday_date)
-# save it in a new env
-# list new env data: ls(envir = .PortfolioEnv) or ls.str(.PortfolioEnv)
-# rm(list = 'portfolio_20230727', envir = .PortfolioEnv)
-# get(ls(envir = .PortfolioEnv, pattern = '27'))
-.PortfolioEnv <- new.env()
-assign(new_name, portfolio, envir = .PortfolioEnv)
-assign(new_name, portfolio)
 
 
 # ! HINT !
@@ -40,28 +32,44 @@ dev.off()
     startTime <- gsub('-', '', start_time)
     endTime <- gsub('-', '', end_time)
 # find our two df in .PortfolioEnv
-    df1 <- grep(startTime, ls(envir = .PortfolioEnv), value = TRUE)
-    df2 <- grep(endTime, ls(envir = .PortfolioEnv), value = TRUE)
+    df1 <- get(grep(startTime, ls(envir = .PortfolioEnv), value = TRUE), envir = .PortfolioEnv)
+    df1 <- mutate(df1, 'asset' = paste0(df1$asset, 'USDT'))
+    df2 <- get(grep(endTime, ls(envir = .PortfolioEnv), value = TRUE), envir = .PortfolioEnv)
+    df2 <- mutate(df2, 'asset' = paste0(df2$asset, 'USDT'))
+
     
 # check if assets have been sold or bought
 # if total.x - total.y != 0, then we have traded. In this case, we need first to adjust quantity as
 # they need to be equal. After we must take into account the trades.
-    portfolio.2dates <- inner_join(.GlobalEnv[[df1]], .GlobalEnv[[df2]], by = 'asset')
-    compare.amount <- mutate(portfolio.2dates, amount = amount.x - amount.y)
-# keep rows with amount !=0 and remove BNB
-    trades <- filter(compare.amount, amount != 0)
-    trades <- filter(trades, asset != 'BNB')
-    trades <- mutate(trades, 'asset' = paste0(trades$asset, 'USDT'))
-# if length(trades$asset) = 0, we didn't trade.
-# ! HINT: one cancelled open order will change quantity but with no trades.
-    if (length(trades$asset) > 0) {
-        past.trades <- lapply(trades$asset, function(x) binance_all_orders(x, start_time = start_time, end_time = end_time))
-        names(past.trades) <- trades$asset
-        past.trades$BTCUSDT %>% filter(time >= start_time & time <= end_time) 
-    }
+# !! we can't exclude trades between the two dates with equal amounts
+#     portfolio.2dates <- inner_join(df1, df2, by = 'asset')
+#     compare.amount <- mutate(portfolio.2dates, amount = amount.x - amount.y)
+# # keep rows with amount !=0 and remove BNB
+#     trades <- filter(compare.amount, amount != 0)
+#     trades <- filter(trades, asset != 'BNB')
+#     trades <- mutate(trades, 'asset' = paste0(trades$asset, 'USDT'))
+# # if length(trades$asset) = 0, we didn't trade.
+# # ! HINT: one cancelled open order will change quantity but with no trades.
+#     if (length(trades$asset) > 0) {
+#         past.trades <- lapply(trades$asset, function(x) binance_all_orders(x, start_time = start_time, end_time = end_time))
+#         names(past.trades) <- trades$asset
+#         past.trades$BTCUSDT %>% filter(time >= start_time & time <= end_time) 
+#     }
 
+    
 # get_trades_pl()
-past.trades <- lapply(mlc$asset.usdt, function(x) binance_all_orders(x))
+    # here we must check our assets in df1 & df2 are the same 
+    if (identical(df1$asset, df2$asset) != TRUE) {
+        stop('Nos assets sont différentes entre les deux dates')
+    }
+    past.trades <- lapply(df1$asset, function(x) binance_all_orders(x))
+    names(past.trades) <- df1$asset
+    # keep our needed period
+    condition_filter <- function(tb) {
+        return(filter(tb, time >= start_time))
+    }
+    past.trades.select <- lapply(past.trades, condition_filter)
+    
 # return last row of each tibbles of the list
 last.rows <- map(past.trades, ~tail(.x, n = 1))
 # keep specific columns
