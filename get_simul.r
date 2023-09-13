6e6796856b483ec3f3757f1c85189e1a9cbc7596
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-09-13 21:31:32 +0200

    a few cosmetic changes

--- get_simul.r
+++ get_simul.r
@@ -8,8 +8,8 @@ portfolio.simul.usd <- portfolio.simul %>%
     mutate(asset = paste(asset, 'USDT', sep = ''))
 
 asset.price <- lapply(portfolio.simul.usd$asset, function(x) binance_klines(
-    x, interval = '1d', limit = 365
-))
+    x, interval = '1d', limit = 365))
+
 close.price <- lapply(asset.price, function(x) pull(x,close))
 
 # function to extract first and last element of a list 
@@ -26,7 +26,7 @@ extract_first_last <- function(x) {
 
 # annualized returns
 close.first.last <- lapply(close.price, function(x) extract_first_last(x))
-return <- map(close.first.last, ~ ((.x[2] / .x[1]) -1) *100)
+return <- map(close.first.last, ~ ((.x[2] / .x[1]) ^ (1 / 1) -1) *100)
 return <- lapply(return, function(x) round(x, digits = 2))
 names(return) <- portfolio.simul$asset
 return <- enframe(return, name = 'asset', value = 'Return')
@@ -52,13 +52,13 @@ vol_ror <- left_join(return, volat, by = 'asset')
 # plot
 # library(RColorBrewer)
 # display.brewer.all(colorblindFriendly = TRUE)
-colourCount = length(vol_ror$asset)
-getPalette = colorRampPalette(brewer.pal(9, "Set1"))
-my.color <- c('#EE8EBF','#25801f','#2E946F','#78C66E','#3D5D78','#D58C50',
+# getPalette = colorRampPalette(brewer.pal(9, "Set1"))
+
+my.color <- c('#EE8EBF','#25801f','#f73905','#78C66E','#3D5D78','#D58C50',
               '#CB88B1','#5B5070','#AA5A74','#8790C1','#54ADD1','#edc161',
               '#078989','#52A98B','#6AF2A5','#B89B44','#DF8052','#BB77A5',
-              '#E4F261','#9c44cf','#AEA33C','#874339','#93D383','#A3C455',
-              '#092129','#C1CD51','#C0E463','#87A04F','#22DFEA','#617197')
+              '#E4F261','#9c44cf','#AEA33C','#874339','#3722d6','#A3C455',
+              '#b80909','#C1CD51','#C0E463','#87A04F','#22DFEA','#617197')
 
 
 
@@ -71,37 +71,37 @@ ggplot(vol_ror, aes(x = Volatility, y = Return)) +
 
 
 #### TEST ###
-ggplot(data = vol_ror, mapping = aes(x = Volatility, y = Return)) + 
-    geom_point(mapping = aes(color = asset), size = 3) +
-    geom_smooth(method = 'lm') +
-    labs(title = 'Volatility vs Return',
-         x = 'Volatility', y = '365 day rate of return',
-         color = 'asset') +
-    scale_color_manual(values = my.color)
-
-
-
-
-
-ggplot(data = vol_ror, mapping = aes(x = Volatility, y = Return)) + 
-    geom_point(aes(color = asset)) +
-    geom_smooth(method = 'lm') 
-
-
-    scale_color_gradientn(colours = rainbow(26))
-
-
-    labs(title = 'Portfolio simulation',
-         x = 'Daily volatility', y = '365 day rate of return',
-         color = 'asset') 
-
-
-
-
-    scale_fill_brewer(palette = "Dark2")
-
-    scale_color_gradient(low = 'greenyellow', high = 'forestgreen')
-
-    scale_color_colorblind() 
+# ggplot(data = vol_ror, mapping = aes(x = Volatility, y = Return)) + 
+#     geom_point(mapping = aes(color = asset), size = 3) +
+#     geom_smooth(method = 'lm') +
+#     labs(title = 'Volatility vs Return',
+#          x = 'Volatility', y = '365 day rate of return',
+#          color = 'asset') +
+#     scale_color_manual(values = my.color)
+# 
+# 
+# 
+# 
+# 
+# ggplot(data = vol_ror, mapping = aes(x = Volatility, y = Return)) + 
+#     geom_point(aes(color = asset)) +
+#     geom_smooth(method = 'lm') 
+# 
+# 
+#     scale_color_gradientn(colours = rainbow(26))
+# 
+# 
+#     labs(title = 'Portfolio simulation',
+#          x = 'Daily volatility', y = '365 day rate of return',
+#          color = 'asset') 
+# 
+# 
+# 
+# 
+#     scale_fill_brewer(palette = "Dark2")
+# 
+#     scale_color_gradient(low = 'greenyellow', high = 'forestgreen')
+# 
+#     scale_color_colorblind() 
 
 #   scale_fill_brewer(palette = "Set3")
