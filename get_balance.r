a71c67bed831545935b65af8511c9d89c0bf8c3e
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-09-15 15:10:49 +0200

    we need to take account the lock column (our stop orders)

--- get_balance.r
+++ get_balance.r
@@ -11,7 +11,7 @@ get_balance <- function () {
 
 # get binance balance without zero
     bin.balance <- as_tibble(binance_balances())
-    bin.balance <- filter(bin.balance, bin.balance$free != 0)
+    bin.balance <- filter(bin.balance, bin.balance$free != 0 | bin.balance$locked != 0)
     bin.balance <- select(bin.balance, -c(free, locked))
     bin.balance <- arrange(bin.balance, asset)
     bin.balance <- filter(bin.balance, !(asset %in% c('ETWH', 'LUNA', 'USDC', 'XRP', 'ETHW')))
@@ -22,15 +22,14 @@ get_balance <- function () {
     balance <- bind_rows(bin.balance, safe.balance) %>%
         group_by(asset) %>%
         summarise(total = sum(total))
-
     mlc.asset <- as.list(balance$asset)
+    
 # remove USDT form our assets & add USDT to each asset name
     mlc.asset <- mlc.asset[-which(mlc.asset == 'USDT')]
     mlc.asset.usdt <- as.list(paste(mlc.asset, 'USDT', sep = ''))
     # mlc.asset.usdt <- mlc.asset.usdt[-which(mlc.asset.usdt == 'USDTUSDT')]
 
     mlc <- list(balance = balance, asset.usdt = mlc.asset.usdt, asset = mlc.asset)
-    return(mlc)
 }
 
 
