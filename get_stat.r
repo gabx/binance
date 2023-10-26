b17ec51dca0797a3d87b0528c6372e8252e47ff7
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-10-26 14:07:34 +0200

    split the function in two, so we can run only the get_quotes() to retrive quotes without stats; add a p for period argument.

--- get_stat.r
+++ get_stat.r
@@ -3,67 +3,67 @@
 # object of class xts input data is organized with asset returns in columns and
 # dates represented in row. xts convenient with PerformanceAnalytics package
 
-get_stat <- function() {
-# Get a list of token as xts objects 
-library(xts)
-library(PerformanceAnalytics)
-library(binancer)
-library(corrplot)
 
-portfolio.simul <- read_csv("simul_small.csv", col_names = 'asset', show_col_types = FALSE)
-portfolio.simul.usd <- portfolio.simul %>% 
-    mutate(asset = paste(asset, 'USDT', sep = ''))
-
-asset.price.list <- lapply(portfolio.simul.usd$asset, function(x) binance_klines(x,
-                            interval = '1d'))
-extract_close <- function(df) {
-    df[, c('close', 'close_time'), drop = FALSE]
-}
-asset.price.close <- lapply(asset.price.list, extract_close)
-
-to_date <- function(df) {
-    df <- df |> mutate(close_time = as.Date(close_time))
-}
-asset.price.close <- lapply(asset.price.close, to_date)
-
-to_xts <- function(df) {
+get_quotes <- function(p) {
+    
+    library(xts)
+    library(PerformanceAnalytics)
+    library(binancer)
+    
+    # return a xts object of returns on a specific period
+    portfolio.simul <- read_csv("simul1.csv", col_names = 'asset', show_col_types = FALSE)
+    portfolio.simul.usd <- portfolio.simul %>% 
+        mutate(asset = paste(asset, 'USDT', sep = ''))
+    asset.price.list <- lapply(portfolio.simul.usd$asset, function(x) binance_klines(x,
+        interval = p , start_time = '2023-10-17', end_time = '2023-10-20'))
+    asset.price.close <- lapply(asset.price.list, function(df) 
+        {df[, c('close', 'close_time'), drop = FALSE]})
+    asset.price.close <- lapply(asset.price.close, function(df)
+        {df <- df |> mutate(close_time = as.Date(close_time))})
+    names(asset.price.close) <- portfolio.simul.usd$asset
+    assign(asset.price.close, 'asset.price.close', envir = .GlobalEnv)
+    to_xts <- function(df) {
     xts::xts(x = df$close, order.by = as.Date(df$close_time))
+    }
+    asset.xts.lst <- lapply(asset.price.close, to_xts)
 }
-asset.xts.lst <- lapply(asset.price.close, to_xts)
-names(asset.xts.lst) <- portfolio.simul.usd$asset
 
-# list of daily returns in xts format
-asset.return.lst <- lapply(asset.xts.lst, Return.calculate)
-# create a xts object with all returns and asset names as colnames
-asset.return.xts <- na.omit(do.call(merge, asset.return.lst))
-assign('asset.return.xts', asset.return.xts, envir = .GlobalEnv)
+get_stat <- function(p) {
+
+    library(xts)
+    library(PerformanceAnalytics)
+    library(corrplot)
+    
+    asset.xts.lst <- get_quotes(p)
+    asset.return.lst <- lapply(asset.xts.lst, Return.calculate)
+    asset.return.xts <- do.call(merge, asset.return.lst)
 
 # Create our final tibble asset.stats
 # 1 - Return.calculate : price to return to return.annualized
-asset.return <- round(sapply(asset.return.lst, Return.annualized) * 100, digits = 2)
-asset.stats <- as_tibble_col(asset.return, column_name = 'Annualized_return')
-asset.stats <- rownames_to_column(asset.stats, var = 'asset')
-asset.stats$asset <- names(asset.xts.lst)
+    asset.return <- round(sapply(asset.return.xts, Return.annualized) * 100, digits = 2)
+    asset.stats <- as_tibble_col(asset.return, column_name = 'Annualized_return')
+    asset.stats <- rownames_to_column(asset.stats, var = 'asset')
+    asset.stats$asset <- names(asset.xts.lst)
 
 # 2 - annualized standard deviation
-asset.stddev <- round(sapply(asset.return.lst, StdDev.annualized) * 100, digits = 2)
-asset.stddev <- as_tibble_col(asset.stddev, column_name = 'Annualized_volatility')
+    asset.stddev <- round(sapply(asset.return.xts, StdDev.annualized) * 100, digits = 2)
+    asset.stddev <- as_tibble_col(asset.stddev, column_name = 'Annualized_volatility')
 
 # 3 - annualized sharpe ratio
-asset.sharpe <- round(sapply(asset.return.lst, SharpeRatio.annualized, scale = 365), digits = 2)
+    asset.sharpe <- round(sapply(asset.return.xts, SharpeRatio.annualized, scale = 365), digits = 2)
 # asset.sharpe <- as_tibble_col(unlist(asset.sharpe), column_name = 'Annualized_sharpe')
-asset.sharpe <- as_tibble_col(asset.sharpe, column_name = 'Annualized_sharpe')
+    asset.sharpe <- as_tibble_col(asset.sharpe, column_name = 'Annualized_sharpe')
 
 # 4 Kurtosis
 # > 3  heavier tails and a higher peak 
 # < 3 lighter tails and a flatter peak
-asset.kurtosis <- round(sapply(asset.return.lst, PerformanceAnalytics::kurtosis), digits = 2)
-asset.kurtosis <- as_tibble_col(asset.kurtosis, column_name = 'Kurtosis')
+    asset.kurtosis <- round(sapply(asset.return.xts, PerformanceAnalytics::kurtosis), digits = 2)
+    asset.kurtosis <- as_tibble_col(asset.kurtosis, column_name = 'Kurtosis')
 
 # 4 - skewness Positive skewness indicates that more of the returns are positive, negative skewness indicates that
 # more of the returns are negative
-asset.skewness <- round(sapply(asset.return.lst, PerformanceAnalytics::skewness), digits = 2)
-asset.skewness <- as_tibble_col(asset.skewness, column_name = 'Skewness')
+    asset.skewness <- round(sapply(asset.return.xts, PerformanceAnalytics::skewness), digits = 2)
+    asset.skewness <- as_tibble_col(asset.skewness, column_name = 'Skewness')
 
 # 5 - semivariance, semideviation ??
 # SemiDeviation and SemiVariance are implemented as a
@@ -71,38 +71,39 @@ asset.skewness <- as_tibble_col(asset.skewness, column_name = 'Skewness')
 # Downside deviation, similar to semi deviation, eliminates positive returns when calculating risk.
 # Instead of using the mean return or zero, it uses the Minimum Acceptable Return as proposed by
 # Sharpe (which may be the mean historical return or zero)
-asset.semidev <- round(sapply(asset.return.lst, SemiDeviation) * 100, digits = 2)
-asset.semidev <- as_tibble_col(asset.semidev, column_name = 'Semi_deviation')
+    asset.semidev <- round(sapply(asset.return.xts, SemiDeviation) * 100, digits = 2)
+    asset.semidev <- as_tibble_col(asset.semidev, column_name = 'Semi_deviation')
 
 # 6 maxdrawdown
-asset.maxdd <- round(sapply(asset.return.lst, maxDrawdown) * 100, digits = 2)
-asset.maxdd <- as_tibble_col(asset.maxdd, column_name = 'max_drawdown')
+    asset.maxdd <- round(sapply(asset.return.xts, maxDrawdown) * 100, digits = 2)
+    asset.maxdd <- as_tibble_col(asset.maxdd, column_name = 'max_drawdown')
 # OR Average drawdown + average recovery period
-asset.avgdd <- round(sapply(asset.return.lst, AverageDrawdown) * 100, digits = 2)
-asset.avgdd <- as_tibble_col(asset.avgdd, column_name = 'Average_drawdown')
-asset.avgrec <- round(sapply(asset.return.lst, AverageRecovery), digits = 2)
-asset.avgrec <- as_tibble_col(asset.avgrec, column_name = 'Average_recovery')
+    asset.avgdd <- round(sapply(asset.return.xts, AverageDrawdown) * 100, digits = 2)
+    asset.avgdd <- as_tibble_col(asset.avgdd, column_name = 'Average_drawdown')
+    asset.avgrec <- round(sapply(asset.return.xts, AverageRecovery), digits = 2)
+    asset.avgrec <- as_tibble_col(asset.avgrec, column_name = 'Average_recovery')
 
 
 # 7 VAR
 # More efficient estimates of VaR are obtained if a (correct) assumption
 # is made on the return distribution, such as the normal distribution. If your return series is skewed
 # and/or has excess kurtosis, Cornish-Fisher estimates of VaR can be more appropriat
-asset.var <- round(sapply(asset.return.lst, VaR, method = 'historical') * 100, digits = 2)
-asset.var <- as_tibble_col(asset.var, column_name = 'Var_historical')
+    asset.var <- round(sapply(asset.return.xts, VaR, method = 'historical') * 100, digits = 2)
+    asset.var <- as_tibble_col(asset.var, column_name = 'Var_historical')
 
 # 8 CVAR or Expected shortfall
-asset.es <- round(sapply(asset.return.lst, ETL, method = 'historical') * 100, digits = 2)
-asset.es <- as_tibble_col(asset.es, column_name = 'Expected_shortfall')
+    asset.es <- round(sapply(asset.return.xts, ETL, method = 'historical') * 100, digits = 2)
+    asset.es <- as_tibble_col(asset.es, column_name = 'Expected_shortfall')
 
 # bind all
-asset.all.stats <- bind_cols(asset.stats, asset.stddev, asset.kurtosis, asset.skewness,
-                             asset.sharpe, asset.semidev, asset.maxdd, asset.var)
+    asset.all.stats <- bind_cols(asset.stats, asset.stddev, asset.kurtosis, asset.skewness,
+                             asset.sharpe, asset.maxdd, asset.var)
 # asset.all.stats <- bind_cols(asset.stats, asset.stddev, asset.sharpe, asset.semidev, asset.avgdd, asset.avgrec, asset.var)
 
 # remove USDT from 1st column
-asset.all.stats <- asset.all.stats %>% mutate(asset = gsub('USDT', '', asset))
+    asset.all.stats <- asset.all.stats %>% mutate(asset = gsub('USDT', '', asset))
 }
+
 
 # corrplot::corrplot(cor(asset.return.xts)): matrice de correlation
 #  table of various risk ratios
