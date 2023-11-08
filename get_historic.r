4577343a3ecacdfe56fb213593fb2d3a32d1c57a
arnaud gaboury <arnaud.gaboury@gmail.com>
2023-11-08 17:03:11 +0100

    change time to UTC from ECT to avoid any issue with daylight saving time

--- get_historic.r
+++ get_historic.r
@@ -9,12 +9,18 @@ get_historic <- function() {
     #interval = '8h', start_time = '2023-07-17', end_time = '2023-07-21'))
     asset.price.list <- lapply(mlc$asset.usdt, function(x) binance_klines(x,
              interval = '8h'))
-    # keep open value for each day 18:00
+    # we need to change time to UTC instead of CET to avoid issue with daylight
+    # saving time
+    change_to_utc <- function(df) {
+        df |> mutate(open_time = with_tz(open_time, tz = 'UTC'))
+    }
+    asset.price.list.utc <- lapply(asset.price.list, change_to_utc)
+    # keep open value for each day 16:00
     keep_daily_open <- function(df) {
-    df[grepl('18:00:00', df$open_time)]
+    df[grepl('16:00:00', df$open_time)]
     }
 
-    asset.price.open <- lapply(asset.price.list, keep_daily_open)
+    asset.price.open <- lapply(asset.price.list.utc, keep_daily_open)
     names(asset.price.open) <- mlc$asset
     # final list of df with open_time, open and symbol columns
     asset.price.open <- map(asset.price.open, ~select(.x, 1, 2, 12))
